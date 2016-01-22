ce37695418295f5996428b7b8a70acf722988aba
Lopi <lototchi@redkiwi.nl>
2016-01-22 11:29:20 +0200

    add mobile menu as duplicate for main menu, to be styled later

--- javascript/general.js
+++ javascript/general.js
@@ -3,6 +3,11 @@ jQuery.noConflict();
 
 jQuery(function($){
 	
+	var $respMenu = $('#main-menu').clone();
+	$respMenu.prop('id','main-menu-mobile');
+	$('body').prepend($respMenu);
+	
+	
 	$('#menu-toggler').on('click', function() {
 		$('body').toggleClass('show-menu');
 	});
