f7a35aa374d9e0caeb32cd94f243e08c335911d6
Lopi <lototchi@redkiwi.nl>
2016-01-25 12:00:01 +0200

    add mobile menu overlay over body basic behaviour

--- javascript/general.js
+++ javascript/general.js
@@ -5,10 +5,12 @@ jQuery(function($){
 	
 	var $respMenu = $('#main-menu').clone();
 	$respMenu.prop('id','main-menu-mobile');
-	$('body').prepend($respMenu);
+	$('body').prepend($respMenu).prepend('<div id="mobile-menu-overlay" style="display:none;">');
 	
 	
 	$('#menu-toggler').on('click', function() {
 		$('body').toggleClass('show-menu');
+		$('#mobile-menu-overlay').toggle(300);
+		
 	});
 });
